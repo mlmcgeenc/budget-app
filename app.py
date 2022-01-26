e6231c960b8518f0f182d53ea5347ea9f1d1593a
Matt McGee <mlmcgeenc@gmail.com>
2022-01-26 18:39:53 -0500

    add withdraw and get_balance methods

--- app.py
+++ app.py
@@ -1,15 +1,27 @@
 class Category:
-  ledger = [1, 2, 3, 4, 5]
+  name = ""
+  ledger = []
 
-  def deposit(self, value):
-    self.ledger.append(value)
-
-
-
+  def __init__(self, name):
+    self.name = name
+  def deposit(self, amount, description=""):
+    self.ledger.append({"amount": amount, "description": description})
+  def withdraw(self, amount, description=""):
+    self.ledger.append({"amount": (-1 * amount), "description": description})
+  def get_balance(self):
+    balance = 0
+    for entry in self.ledger:
+      balance = balance + entry['amount']
+    return balance
 
 def create_spend_chart(categories):
   return 'Chart not yet ready'
 
-budget = Category()
-budget.deposit(6)
-print(budget.ledger)
+budget = Category('My Budget')
+budget.deposit(50, 'deposit')
+budget.deposit(200)
+budget.withdraw(50, "milk, cereal, eggs, bacon, bread")
+budget.withdraw(50)
+print(budget.get_balance())
+print(budget.name)
+print(budget.ledger[0])
