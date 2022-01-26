60af24cade069b4ea0a31a43fe4d44606ba0d4c1
Matt McGee <mlmcgeenc@gmail.com>
2022-01-25 19:19:10 -0500

    add a method

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 class Category:
+  ledger = [1, 2, 3, 4, 5]
 
+  def deposit(self, value):
+    self.ledger.append(value)
 
 
 
 
 def create_spend_chart(categories):
+  return 'Chart not yet ready'
+
+budget = Category()
+budget.deposit(6)
+print(budget.ledger)
