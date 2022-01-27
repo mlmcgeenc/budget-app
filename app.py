e0a3d0e2fc6c0437da3097e96fb63c5b151b3cc9
Matt McGee <mlmcgeenc@gmail.com>
2022-01-27 08:36:23 -0500

    add check_funds and transfer methods

--- app.py
+++ app.py
@@ -4,24 +4,66 @@ class Category:
 
   def __init__(self, name):
     self.name = name
-  def deposit(self, amount, description=""):
-    self.ledger.append({"amount": amount, "description": description})
-  def withdraw(self, amount, description=""):
-    self.ledger.append({"amount": (-1 * amount), "description": description})
+
   def get_balance(self):
     balance = 0
     for entry in self.ledger:
-      balance = balance + entry['amount']
+      balance = balance + entry["amount"]
     return balance
 
-def create_spend_chart(categories):
-  return 'Chart not yet ready'
+  def check_funds(self, check):
+    balance = self.get_balance()
+    if balance > check:
+      return True
+    else:
+      return False
 
-budget = Category('My Budget')
-budget.deposit(50, 'deposit')
-budget.deposit(200)
-budget.withdraw(50, "milk, cereal, eggs, bacon, bread")
+  def deposit(self, amount, description=""):
+    self.ledger.append({"amount": amount, "description": description})
+
+  def withdraw(self, amount, description=""):
+    if self.check_funds(amount) == True:
+      self.ledger.append({"amount": (-1 * amount), "description": description})
+      return True
+    else:
+      return False
+
+  def transfer(self, amount, category=""):
+    if self.check_funds(amount) == True:
+      self.withdraw(amount, "Transfer to " + str(category))
+      secondBudget.deposit(amount, "Transfer from " + str(self.name))
+      return True
+    else:
+      return False
+
+  def print_ledger(self):
+    for entry in self.ledger:
+      print(entry)
+
+def create_spend_chart(categories):
+  return "Chart not yet ready"
+
+budget = Category("Test Budget")
+secondBudget = Category("Second_Budget")
+print("Budget created:")
+print(budget)
+print("Deposit:")
+budget.deposit(100, "deposit")
+budget.print_ledger()
+print("Deposit - no description:")
+budget.deposit(100)
+budget.print_ledger()
+print("Withdrawal:")
+budget.withdraw(50, "withdrawal")
+budget.print_ledger()
+print("Withdrawal - no desc:")
 budget.withdraw(50)
-print(budget.get_balance())
-print(budget.name)
-print(budget.ledger[0])
+budget.print_ledger()
+print("Transfer:")
+budget.transfer(50, "Second_Budget")
+budget.print_ledger()
+print("Overdrawn:")
+budget.withdraw(200)
+budget.print_ledger()
+print("Second Budget:")
+secondBudget.print_ledger()
